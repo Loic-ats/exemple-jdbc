f36fe6034e97abf705ece786ad8c8f6517864a93
Loïc ARTUSO <artusol@hotmail.fr>
2020-07-24 11:34:37 +0200

    TP JDBC

---
+++ src/main/java/exemple/jdbc/TestConnectionJdbc.java
@@ -0,0 +1,64 @@
+package exemple.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+
+public class TestConnectionJdbc {
+
+	public static void main(String[] args) {
+
+        // recupere le fichier properties
+        ResourceBundle db = ResourceBundle.getBundle("database2");
+        Connection connection = null;     
+        // creer la connection
+        try {	
+        	//enregistre le pilote
+        	Class.forName(db.getString("db.driver"));    	    	
+        	//Connection connection = null
+        	//Crer la connection	   	
+        	connection = DriverManager.getConnection(
+        			db.getString("db.url"), 
+        			db.getString("db.user"),
+                    db.getString("db.pass")); 	
+       
+        	 // affiche la connexion
+            boolean valid = connection.isValid(500);
+            if (valid) {
+                // SEVERE = Erreur
+                // INFO = info
+                // WARNING = Avertissement
+                // FINEST = Debug
+                System.out.println("La connection est ok");
+            } else {
+                System.out.println("Il y a une erreur de connection");             
+            }
+        }    
+            catch (SQLException | ClassNotFoundException e) {
+        // Handle errors for JDBC
+        System.out.println("Erreur de connection : " + e.getMessage());
+    }
+            
+            finally {
+            	try {
+            		
+            		if(connection != null) connection.close();
+                      	
+                } catch(SQLException e) {
+            	
+      
+                	 System.out.println("Erreur de fermeture : " + e.getMessage());
+                }
+            	
+            	}
+            System.out.println("Base déconnectée ! "); 
+        }
+	}
+
+
+
+
+
+
